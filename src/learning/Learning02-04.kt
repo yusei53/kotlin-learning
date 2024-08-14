fedfb31efe59acf6cda298ea8fdcceee982bb7b7
yusei53 <yusei530922@gmail.com>
2024-08-15 00:43:17 +0900

    add Learning02-04.kt

---
+++ src/learning/Learning02-04.kt
@@ -0,0 +1,13 @@
+package learning
+
+fun main(args: Array<String>) {
+    val message: String? = readLine()
+    val number: Int = Integer.parseInt(message)
+
+
+    for (i in 1..number) {
+        val message: String? = readLine()
+        println(message)
+    }
+
+}
