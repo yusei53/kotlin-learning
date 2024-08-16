94bd9ec56caf2c04fabba7632ca9ebda41e50d43
yusei53 <yusei530922@gmail.com>
2024-08-16 16:27:58 +0900

    add Learning02-08.kt

---
+++ src/learning/Learning02-08.kt
@@ -0,0 +1,9 @@
+package learning
+
+fun main(args: Array<String>) {
+    val number = readLine()!!.toInt()
+    val text = readLine()!!.split(",")
+    for (i in 0..number-1){
+        println(text[i])
+    }
+}
