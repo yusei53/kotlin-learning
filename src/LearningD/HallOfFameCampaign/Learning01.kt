b436298816db7470da5d39524d198b5e2b2197f0
yusei53 <yusei530922@gmail.com>
2024-08-18 19:52:22 +0900

    add Learning01.kt in LearningD.HallOfFameCampaign

---
+++ src/LearningD/HallOfFameCampaign/Learning01.kt
@@ -0,0 +1,10 @@
+package LearningD.HallOfFameCampaign
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+//    for (i in 1..n) {
+//        print("*")
+//    }
+    print("*".repeat(n))
+
+}
