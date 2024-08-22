55c7f2743c3f010a690d5ac0ff8ec7a50f21066b
yusei53 <yusei530922@gmail.com>
2024-08-22 18:31:56 +0900

    add Learning03.kt in LearningC.DataSetSelection

---
+++ src/LearningC/DataSetSelection/Learning03.kt
@@ -0,0 +1,9 @@
+package LearningC.DataSetSelection
+
+fun main(args: Array<String>) {
+    val N = readLine()!!.split(" ").map { it.toInt() }
+    val min = N.min()
+    val max = N.max()
+
+    print(max - min)
+}
