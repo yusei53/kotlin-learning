620c0d79cd92f21bb045114d59bd8497a46338ed
yusei53 <yusei530922@gmail.com>
2024-08-25 22:17:57 +0900

    add Learning08.kt in LearningC.DataSetSelection

---
+++ src/LearningC/DataSetSelection/Learning08.kt
@@ -0,0 +1,12 @@
+package LearningC.DataSetSelection
+
+fun main(args: Array<String>) {
+    val N = readLine()!!.toInt()
+    val A = readLine()!!.split(" ").map { it.toInt() }
+
+    val counts = IntArray(10)
+    for (i in A) {
+        counts[i]++
+    }
+    print(counts.joinToString(" "))
+}
