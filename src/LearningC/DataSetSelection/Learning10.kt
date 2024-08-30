96db82cf01c7fea8ee9fba24b7671abc314e803d
yusei53 <yusei530922@gmail.com>
2024-08-30 15:45:13 +0900

    add Learning10.kt in LearningC.DataSetSelection

---
+++ src/LearningC/DataSetSelection/Learning10.kt
@@ -0,0 +1,22 @@
+package LearningC.DataSetSelection
+
+fun main(args: Array<String>) {
+    val N = readLine()!!.toInt()
+    val StringArray = mutableListOf<String>()
+
+    repeat(N) {
+        StringArray.add(readLine()!!)
+    }
+
+    // コレクションをグループ化して、各要素の出現回数をカウント
+    val groupList = StringArray.groupingBy { it }.eachCount()
+
+    // グループ化したリストをソート
+    val sortGroupList = groupList.toSortedMap()
+
+    // Map型なのでkey-valueを引数に取ることができる
+    sortGroupList.forEach { (key, value) ->
+        println("$key $value")
+    }
+
+}
