a99991b58c7ed0a962f7e10f3c26218184c0129d
yusei53 <yusei530922@gmail.com>
2024-09-05 23:59:36 +0900

    update Learning04.kt in LearningC.DataSetSelection

--- src/LearningC/DataSetSelection/Learning04.kt
+++ src/LearningC/DataSetSelection/Learning04.kt
@@ -2,14 +2,13 @@ package LearningC.DataSetSelection
 
 fun main(args: Array<String>) {
     val (n1, n2) = readLine()!!.split(" ").map { it.toInt() }
-    val list = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
-
+    val numberList = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
     repeat(n2) {
-        val query = readLine()!!.split(" ").map { it.toInt() }
-        when (query[0]) {
-            0 -> list.add(query[1])
-            1 -> list.removeLast()
-            2 -> println(list.joinToString(" "))
+        val result = readLine()!!.split(" ").map { it.toInt() }
+        when (result[0]) {
+            0 -> numberList.add(result[1])
+            1 -> numberList.removeLast()
+            2 -> println(numberList.joinToString(" "))
         }
     }
 }
