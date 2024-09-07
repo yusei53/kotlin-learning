6f5a7abd3cf92b378579d7da54df7f3895cb7677
yusei53 <yusei530922@gmail.com>
2024-09-08 00:32:19 +0900

    update Learning07.kt in LearningC.DataSetSelection to be two requirement

--- src/LearningC/DataSetSelection/Learning07.kt
+++ src/LearningC/DataSetSelection/Learning07.kt
@@ -2,11 +2,8 @@ package LearningC.DataSetSelection
 
 fun main(args: Array<String>) {
     val (n1, n2, n3, n4) = readLine()!!.split(" ").map { it.toInt() }
-    val mass = List(n1) { readLine()!! }
-    val selectedMass = mass[n3 - 1][n4 - 1]
-    val result = when (selectedMass) {
-        '#' -> "Yes"
-        else -> "No"
-    }
-    println(result)
+    val roadList = List(n1) { readLine()!! }
+    val road = roadList[n3 - 1][n4 - 1]
+    val result = if (road == '#') "Yes" else "No"
+    print(result)
 }
