fdbf36e8efda97cfc94f233b9f3a63117e144a27
yusei53 <yusei530922@gmail.com>
2024-09-08 11:30:40 +0900

    update Learning09.kt in LearningC.DataSetSelection

--- src/LearningC/DataSetSelection/Learning09.kt
+++ src/LearningC/DataSetSelection/Learning09.kt
@@ -3,11 +3,13 @@ package LearningC.DataSetSelection
 fun main(args: Array<String>) {
     val N = readLine()!!.toInt()
     val S: List<Char> = readLine()!!.toList()
+    // val charList = readLine()!!.toCharArray()
 
     // アルファベットってASCIIコードで数値として計算できるのか。。
     val counts = IntArray(26)
     for (i in S) {
         counts[i - 'a']++
+        // counts[97 - 97]++
     }
 
     print(counts.joinToString(" "))
