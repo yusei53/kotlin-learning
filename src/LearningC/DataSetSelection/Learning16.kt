41724c3f7e404a133e350507a1b295e3bbd3608e
yusei53 <yusei530922@gmail.com>
2024-09-12 21:34:24 +0900

    add Learning16.kt in LearningC.DataSetSelection

---
+++ src/LearningC/DataSetSelection/Learning16.kt
@@ -0,0 +1,13 @@
+package LearningC.DataSetSelection
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    val firstList = readLine()!!.split(" ").map { it.toInt() }
+    val secondList = readLine()!!.split(" ").map { it.toInt() }
+
+    // listの結合
+    val joinedList = firstList + secondList
+    // 重複を削除してソート
+    val correctList = joinedList.toSet().sorted()
+    print(correctList.joinToString(" "))
+}
