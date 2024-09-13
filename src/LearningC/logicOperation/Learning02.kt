d00c31de46229fd8ee500e1c79f7f8b8d51c4e65
yusei53 <yusei530922@gmail.com>
2024-09-13 23:30:10 +0900

    add Learning02.kt in learningC.logicOperation

---
+++ src/LearningC/logicOperation/Learning02.kt
@@ -0,0 +1,13 @@
+package learningC.logicOperation
+
+fun main(args: Array<String>) {
+    val (n1, n2) = readLine()!!.split(" ").map { it.toInt() }
+    if (n1 == 0 && n2 == 0) {
+        print(0)
+    } else {
+        print(1)
+    }
+
+    // ビット演算の論理演算子orでもいける
+    // print(n1 or n2)
+}
