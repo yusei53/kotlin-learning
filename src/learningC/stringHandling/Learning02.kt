42bda905280988a5f390c58df39a5dd718a64388
yusei53 <yusei530922@gmail.com>
2024-10-12 11:33:16 +0900

    add Learning02.kt in learningC.stringHandling

---
+++ src/learningC/stringHandling/Learning02.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val string = readln().split("/")
+    val year = string[0]
+    val month = string[1]
+    val date = string[2]
+
+    val time = string[3].split(":")
+    val hour = time[0]
+    val minute = time[1]
+
+    println(year)
+    println(month)
+    println(date)
+    println(hour)
+    println(minute)
+}
