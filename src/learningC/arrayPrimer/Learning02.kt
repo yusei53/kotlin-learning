6c9d17f70cdda3e7a012b12239fcf2d8d8615bf5
yusei53 <yusei530922@gmail.com>
2024-11-09 14:44:13 +0900

    add Learning02.kt in learningC.arrayPrimer

---
+++ src/learningC/arrayPrimer/Learning02.kt
@@ -0,0 +1,9 @@
+package learningC.arrayPrimer
+
+fun main(args: Array<String>) {
+    val array = listOf(5, 1, 3, 4, 5, 12, 6, 8, 1, 3)
+    array.forEach { element ->
+        println(element)
+
+    }
+}
