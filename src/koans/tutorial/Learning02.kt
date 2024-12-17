a97d84bbe4b176948a921e4ddee6afbea835ecb3
yusei53 <yusei530922@gmail.com>
2024-12-17 16:12:21 +0900

    add Learning02.kt in koans.tutorial

---
+++ src/koans/tutorial/Learning02.kt
@@ -0,0 +1,16 @@
+package koans.tutorial
+
+// デフォルト引数
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+    (if (toUpperCase) name.uppercase() else name) + number
+
+fun useFoo() = listOf(
+    foo("a"),
+    foo("b", number = 1),
+    foo("c", toUpperCase = true),
+    foo(name = "d", number = 2, toUpperCase = true)
+)
+
+fun main() {
+    useFoo()
+}
