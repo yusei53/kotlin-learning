ccca93f8a8ea6916e2481fb6bbd46549af07c8b1
yusei53 <yusei530922@gmail.com>
2024-12-24 20:11:00 +0900

    feat: TypeIntro.kt

---
+++ src/kotlinProgrammingBook/chapter2/TypeIntro.kt
@@ -0,0 +1,13 @@
+package kotlinProgrammingBook.chapter2
+
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main() {
+    val name = "Kotlin"
+    var points = 5
+    points += 5
+    println(name)
+    println(points)
+
+    val hasSeed = false
+}
