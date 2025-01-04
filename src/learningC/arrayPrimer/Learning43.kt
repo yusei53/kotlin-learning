743484e3fb1bef11279071b56b635247bb5db976
yusei53 <yusei530922@gmail.com>
2025-01-04 20:19:00 +0900

    add Learning43.kt in learningC.arrayPrimer

---
+++ src/learningC/arrayPrimer/Learning43.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val list = listOf(
+        "good",
+        "morning",
+        "paiza",
+        "813",
+        "pa13"
+    )
+
+    val answer = list[2]
+
+    println(answer)
+}
