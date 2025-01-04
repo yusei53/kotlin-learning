984ee25d693e02ef247cc31ab5d8ee1cb1168825
yusei53 <yusei530922@gmail.com>
2025-01-04 20:18:43 +0900

    add Learning41.kt in learningC.arrayPrimer

---
+++ src/learningC/arrayPrimer/Learning41.kt
@@ -0,0 +1,13 @@
+// 自分の得意な言語で
+// Let's チャレンジ！！
+fun main(args: Array<String>) {
+    val list = listOf(
+        "good",
+        "morning",
+        "paiza",
+        "813",
+        "pa13"
+    )
+
+    println(list.size)
+}
