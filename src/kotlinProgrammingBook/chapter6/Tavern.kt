099c2551045bbccdd742aafaa993ba4902b8a01c
yusei53 <yusei530922@gmail.com>
2025-01-19 08:02:27 +0900

    feat: Tavern.kt

---
+++ src/kotlinProgrammingBook/chapter6/Tavern.kt
@@ -0,0 +1,37 @@
+package kotlinProgrammingBook.chapter6
+
+const val TAVERN_NAME = "Taernyl's Folly"
+
+fun main() {
+    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
+}
+
+fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+    } else {
+        "Madrigal says: Thanks for the $name."
+    }
+    println(phrase)
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiouAEIOU]")) {
+        when (it.value) {
+            "a", "A" -> "4"
+            "e", "E" -> "3"
+            "i", "I" -> "1"
+            "o", "O" -> "0"
+            "u", "U" -> "|_|"
+            else -> it.value
+        }
+    }
