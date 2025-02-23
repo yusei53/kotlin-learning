20fc08f97ee8b7a7fee631458e2a1e72286d4098
yusei53 <yusei530922@gmail.com>
2025-02-23 17:32:05 +0900

    fix Learning20.kt in learningC.arrayUtilization

--- src/learningC/arrrayUtilization/learning20.kt
+++ src/learningC/arrrayUtilization/learning20.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val (x, y) = readln().split(" ").map { it.toInt() }
     val list = MutableList(x) { readln().toInt() }
-    val result = list.dropWhile { it <= y }
+    val result = list.filter { it >= y }
 
     result.forEach {
         println(it)
